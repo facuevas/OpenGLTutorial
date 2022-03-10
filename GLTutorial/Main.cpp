f444a216f5fcc458d462c1d2a7fca1aa3d0f284a
Francis Angelo Cuevas <franciscuevas4@gmail.com>
2022-03-09 21:11:57 -0800

    Sin Wave To make triangle "bright up" with the color green.

--- GLTutorial/Main.cpp
+++ GLTutorial/Main.cpp
@@ -23,15 +23,10 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
-const char *fragmentShaderSource1 = "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                    "}\n\0";
 
 const char *fragmentShaderSource2 = "#version 330 core\n"
                                     "out vec4 FragColor;\n"
+                                    "uniform vec4 ourColor;\n"
                                     "void main()\n"
                                     "{\n"
                                     "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
